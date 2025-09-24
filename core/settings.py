1e173d35800bd89831469b94c65ebe52e2533d11
SaschaGerspach <sascha.gerspach@gmx.de>
2025-09-24 11:19:15 +0000

    made app ready for server

--- core/settings.py
+++ core/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = 'django-insecure-!#lfjuv(8o$_j3yeh(+5rau5c@ooxps_sjf)8-ipnp8p(vmd-#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = [".sascha-gerspach.de", "sascha-gerspach.de", "35.198.103.133", "localhost", "127.0.0.1" ]
 
+CSRF_TRUSTED_ORIGINS = [ "https://sascha-gerspach.de", "https://api.coderr.sascha-gerspach.de", "http://35.198.103.133", "https://35.198.103.133"]
 
 # Application definition
 
@@ -35,7 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
-    "django.contrib.messages",
+   "django.contrib.messages",
     "django.contrib.staticfiles",
 
     # project apps
--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,7 +28,7 @@ urlpatterns = [
     path("api/", include("profiles.api.urls")),
     path("api/", include("reviews.api.urls")),
     path('api/', include('user_auth_app.api.urls')),
-]
+] + staticfiles_urlpatterns()
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
